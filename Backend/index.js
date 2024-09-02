7d6ceeb9b547ae9b58073f2deb9a3e06738a16cb
Deepa Bhatnagar <rishabh15.bhatnagar@gmail.com>
2024-09-02 18:27:24 +0530

    project deployed

--- Backend/index.js
+++ Backend/index.js
@@ -19,8 +19,10 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/user.route.js";
+import path from "path";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
+
 //const app = express();
 dotenv.config();
 app.use(express.json());
@@ -43,6 +45,18 @@ console.log(error);
 //routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+// ------------------------ code for deployment -------------------------------
+
+if (process.env.NODE_ENV==='production'){
+  const dirPath = path.resolve();
+
+  app.use(express.static("./Frontend/dist"));
+  app.get("*", (req,res) =>{
+    res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
+  })
+}
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
